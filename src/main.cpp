26ab328447518b1a9ae48366d8dd597c5ecde854
vedant527 <vedant.bhat527@gmail.com>
2023-12-22 14:00:39 -0500

    times diff between ek and ff

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,46 @@
 #include <iostream>
-#include "UnweightedGraph.hpp"
+#include <chrono>
+
 #include "GraphAgent.hpp"
+#include "UnweightedGraph.hpp"
 
 int main() {
-    WeightedGraph g = WeightedGraph(1000);
-    unsigned seed = time(0);
-    srand(seed);
+    int N = 10;
+    int NUM_ITS = 1000;
 
-    int random_edge_count = rand() % 10000;
-    for (int i = 0; i < random_edge_count; i++) {
-        int v1 = rand() % 20;
-        int v2 = rand() % 20;
-        g.addEdge(v1, v2, rand() % 10);
-    }
-    // g.printGraph();
-    GraphAgent b;
-    int parents1[g.getNumVertices()];
-    int parents2[g.getNumVertices()];
+    WeightedGraph g = WeightedGraph(N);
+    GraphAgent agent;
 
-    int start = 0;
-    int end = 2;
-    int x = b.runBfs(g, start, end, parents1);
-    int y = b.runDfs(g, start, end, parents2);
-    int flow1 = b.fordFulkerson(g, start, end);
-    int flow2 = b.edmondsKarp(g, start, end);
+    float ff_time = 0;
+    float ek_time = 0;
 
-    std::cout << "MAX FLOW FF: " << flow1 << " MAX FLOW EK: " << flow2 << std::endl;
-
-    if (x == 1) { 
-        std::cout << "BFS found path! ";
-        int curr = end;
-        std::cout << "path: ";
-        while (curr != -1) {
-            std::cout << curr << " ";
-            curr = parents1[curr];
+    for (int i = 0; i < NUM_ITS; i++) {
+        unsigned seed = time(0);
+        srand(seed);
+        int random_edge_count = rand() % (N * N);
+        for (int i = 0; i < random_edge_count; i++) {
+            int v1 = rand() % N;
+            int v2 = rand() % N;
+            g.addEdge(v1, v2, rand() % 10);
         }
-        std::cout << std::endl;
-    } else {
-        std::cout << "BFS path not found!" << std::endl;
+
+        int parents1[g.getNumVertices()];
+        int parents2[g.getNumVertices()];
+        int v1 = rand() % g.getNumVertices();
+        int v2 = rand() % g.getNumVertices();
+
+        auto start = std::chrono::high_resolution_clock::now();
+        int f1 = agent.fordFulkerson(g, v1, v2);
+        auto end = std::chrono::high_resolution_clock::now();
+        ff_time += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+        start = std::chrono::high_resolution_clock::now();
+        int f2 = agent.edmondsKarp(g, v1, v2);
+        end = std::chrono::high_resolution_clock::now();
+        ek_time += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+        std::cout << i << std::endl;
     }
 
-    if (y == 1) { 
-        std::cout << "DFS found path! ";
-        int curr = end;
-        std::cout << "path: ";
-        while (curr != -1) {
-            std::cout << curr << " ";
-            curr = parents2[curr];
-        }
-        std::cout << std::endl;
-    } else {
-        std::cout << "DFS path not found!" << std::endl;
-    }
+    std::cout << "Ford Fulkerson took: " << ff_time << "us and Edmonds Karp took " << ek_time << "us" << std::endl;
 }
